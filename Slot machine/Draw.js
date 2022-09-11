b59890e7feea3ac6cc84eb9605e5579114d68570
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-09-11 10:20:13 +0200

    Add class Draw

---
+++ Slot machine/Draw.js
@@ -0,0 +1,22 @@
+class Draw {
+  constructor() {
+    this.options = ["red", "green", "blue"];
+    let _result = this.drawResult();
+
+    this.getDrawResult = () => {
+      return _result;
+    };
+  }
+
+  drawResult() {
+    let colors = [];
+    for (let i = 0; i < this.options.length; i++) {
+      const index = Math.floor(Math.random() * this.options.length);
+      const color = this.options[index];
+      colors.push(color);
+    }
+    return colors;
+  }
+}
+
+const draw = new Draw();
--- Slot machine/Statistics.js
+++ Slot machine/Statistics.js
@@ -14,7 +14,6 @@ class Statistics {
       win,
       bid,
     };
-    // console.log(gameResult);
     this.gameResults.push(gameResult);
   }
 
@@ -22,7 +21,6 @@ class Statistics {
     let games = this.gameResults.length;
     let wins = this.gameResults.filter((result) => result.win).length;
     let losses = this.gameResults.filter((result) => !result.win).length;
-    console.log(losses);
 
     return [games, wins, losses];
   }
