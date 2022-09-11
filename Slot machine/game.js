7eb0024252a965f1356adb7191fa0e8b81e46757
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-09-11 11:02:21 +0200

    Add class Game

---
+++ Slot machine/game.js
@@ -0,0 +1,42 @@
+class Game {
+  constructor(start) {
+    this.stats = new Statistics();
+    this.wallet = new Wallet(start);
+
+    document.getElementById("start").addEventListener("click", this.startGame);
+    this.spanWallet = document.querySelector(".panel span.wallet");
+    this.boards = [...document.querySelectorAll("div.color")];
+    this.inputBid = document.getElementById("bid");
+    this.spanResult = document.querySelector(".score span.result");
+    this.spanGames = document.querySelector(".score span.number");
+    this.spanWins = document.querySelector(".score span.win");
+    this.spanLosses = document.querySelector(".score span.lost");
+
+    this.render();
+  }
+
+  render(
+    colors = ["grey", "grey", "grey"],
+    money = this.wallet.getWalletValue(),
+    result = "",
+    wonMoney = 0,
+    bid = 0,
+    stats = [0, 0, 0]
+  ) {
+    this.boards.forEach((board, i) => {
+      board.style.backgroundColor = colors[i];
+    });
+    this.spanWallet.textContent = money;
+    if (result) {
+      result = `You win ${wonMoney}$, `;
+    } else if (!result && result != "") {
+      result = `You lost ${bid}$, `;
+    }
+    this.spanResult.textContent = result;
+    this.spanGames.textContent = stats[0];
+    this.spanWins.textContent = stats[1];
+    this.spanLosses.textContent = stats[2];
+  }
+
+  startGame() {}
+}
