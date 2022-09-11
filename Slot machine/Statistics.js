95ff582cf79f291889c17bc7cbf03a8f7a847011
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-09-11 10:05:16 +0200

    Add class Statistics

---
+++ Slot machine/Statistics.js
@@ -0,0 +1,31 @@
+class Statistics {
+  constructor() {
+    this.gameResults = [
+      { win: true, bind: 2 },
+      { win: false, bind: -4 },
+      { win: true, bind: 2 },
+      { win: false, bind: -4 },
+      { win: true, bind: 8 },
+    ];
+  }
+
+  addGameToStatistics(win, bid) {
+    let gameResult = {
+      win,
+      bid,
+    };
+    // console.log(gameResult);
+    this.gameResults.push(gameResult);
+  }
+
+  showGameStatistics() {
+    let games = this.gameResults.length;
+    let wins = this.gameResults.filter((result) => result.win).length;
+    let losses = this.gameResults.filter((result) => !result.win).length;
+    console.log(losses);
+
+    return [games, wins, losses];
+  }
+}
+
+const stats = new Statistics();
